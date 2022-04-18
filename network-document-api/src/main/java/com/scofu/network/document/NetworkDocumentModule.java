cf7d25b6a2b954be13c64369fa29d2646b351fc1
Jesper <jesper@scofu.com>
2022-04-18 10:13:33 +0200

    remove lazy documents (moved to common)

--- network-document-api/src/main/java/com/scofu/network/document/NetworkDocumentModule.java
+++ network-document-api/src/main/java/com/scofu/network/document/NetworkDocumentModule.java
@@ -3,7 +3,6 @@ package com.scofu.network.document;
 import com.scofu.common.inject.AbstractFeatureModule;
 import com.scofu.common.inject.annotation.Module;
 import com.scofu.network.document.internal.InternalDocumentModule;
-import com.scofu.network.document.lazy.LazyDocumentModule;
 
 /**
  * Network document module.
@@ -14,6 +13,5 @@ public class NetworkDocumentModule extends AbstractFeatureModule {
   @Override
   protected void configure() {
     install(new InternalDocumentModule());
-    install(new LazyDocumentModule());
   }
 }
