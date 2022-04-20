58620a78d69449a4b88062af92e837fd09194533
Jesper <jesper@scofu.com>
2022-04-20 14:11:11 +0200

    Update Document.java

--- network-document-api/src/main/java/com/scofu/network/document/Document.java
+++ network-document-api/src/main/java/com/scofu/network/document/Document.java
@@ -1,6 +1,14 @@
 package com.scofu.network.document;
 
+import com.jsoniter.annotation.JsonProperty;
+
 /**
  * An identifiable and queryable document.
  */
-public interface Document extends Identifiable<String> {}
+public interface Document extends Identifiable<String> {
+
+  @Override
+  @JsonProperty("_id")
+  String id();
+
+}
