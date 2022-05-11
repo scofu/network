6e7b4453e72ac66d33483995cfb5f94d7bc81de8
Jesper <jesper@scofu.com>
2022-05-11 14:20:30 +0200

    make sure stop command is dispatched on main thread

--- network-instance-bukkit/src/main/java/com/scofu/network/instance/bukkit/ShutdownAttemptTask.java
+++ network-instance-bukkit/src/main/java/com/scofu/network/instance/bukkit/ShutdownAttemptTask.java
@@ -3,30 +3,35 @@ package com.scofu.network.instance.bukkit;
 import com.google.inject.Inject;
 import com.scofu.common.inject.Feature;
 import com.scofu.network.instance.bukkit.event.ShutdownAttemptEvent;
+import org.bukkit.Server;
 import org.bukkit.plugin.Plugin;
 
 final class ShutdownAttemptTask implements Runnable, Feature {
 
   private final Plugin plugin;
+  private final Server server;
 
   @Inject
-  ShutdownAttemptTask(Plugin plugin) {
+  ShutdownAttemptTask(Plugin plugin, Server server) {
     this.plugin = plugin;
+    this.server = server;
   }
 
   @Override
   public void enable() {
     final var delay = 20 * 60; // 60 seconds
-    plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, this, delay, delay);
+    server.getScheduler().runTaskTimerAsynchronously(plugin, this, delay, delay);
   }
 
   @Override
   public void run() {
     final var event = new ShutdownAttemptEvent();
-    plugin.getServer().getPluginManager().callEvent(event);
+    server.getPluginManager().callEvent(event);
     if (event.isCancelled()) {
       return;
     }
-    plugin.getServer().dispatchCommand(plugin.getServer().getConsoleSender(), "stop");
+    server.getScheduler()
+        .runTask(plugin,
+            () -> server.dispatchCommand(plugin.getServer().getConsoleSender(), "stop"));
   }
 }
