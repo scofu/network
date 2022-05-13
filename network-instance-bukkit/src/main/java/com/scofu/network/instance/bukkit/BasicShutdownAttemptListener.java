475ea2b2e8215d737d0f9c4896cf84ba25ddab5b
Jesper <jesper@scofu.com>
2022-05-13 16:41:45 +0200

    Update BasicShutdownAttemptListener.java

--- network-instance-bukkit/src/main/java/com/scofu/network/instance/bukkit/BasicShutdownAttemptListener.java
+++ network-instance-bukkit/src/main/java/com/scofu/network/instance/bukkit/BasicShutdownAttemptListener.java
@@ -5,6 +5,7 @@ import com.scofu.common.inject.Feature;
 import com.scofu.network.instance.bukkit.event.ShutdownAttemptEvent;
 import org.bukkit.Server;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 final class BasicShutdownAttemptListener implements Listener, Feature {
@@ -16,7 +17,7 @@ final class BasicShutdownAttemptListener implements Listener, Feature {
     this.server = server;
   }
 
-  @EventHandler
+  @EventHandler(priority = EventPriority.LOWEST)
   private void onShutdownAttemptEvent(ShutdownAttemptEvent event) {
     if (server.getOnlinePlayers().size() != 0) {
       event.setCancelled(true);
