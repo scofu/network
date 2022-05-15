0a99664579c6c463e8858f4edc9bbd5ef02f0fcb
Jesper <jesper@scofu.com>
2022-05-15 13:34:38 +0200

    set default theme before announcing instance creation

--- network-instance-bukkit/src/main/java/com/scofu/network/instance/bukkit/LocalInstanceAnnouncer.java
+++ network-instance-bukkit/src/main/java/com/scofu/network/instance/bukkit/LocalInstanceAnnouncer.java
@@ -6,6 +6,7 @@ import com.google.inject.Inject;
 import com.scofu.common.inject.Feature;
 import com.scofu.network.instance.Instance;
 import com.scofu.network.instance.InstanceRepository;
+import com.scofu.network.instance.SystemRepository;
 import com.scofu.network.instance.api.InstanceAvailabilityReply;
 import com.scofu.network.instance.api.InstanceAvailabilityRequest;
 import com.scofu.network.instance.api.InstanceCreatedMessage;
@@ -13,6 +14,7 @@ import com.scofu.network.instance.bukkit.event.AvailabilityCheckEvent;
 import com.scofu.network.message.MessageFlow;
 import com.scofu.network.message.MessageQueue;
 import com.scofu.network.message.QueueBuilder;
+import com.scofu.text.ThemeRegistry;
 import java.net.InetAddress;
 import java.util.concurrent.CompletableFuture;
 import javax.inject.Named;
@@ -25,20 +27,31 @@ final class LocalInstanceAnnouncer implements Feature {
   private final CompletableFuture<Instance> instanceFuture;
   private final InstanceRepository instanceRepository;
   private final LocalInstanceProvider instanceProvider;
+  private final SystemRepository systemRepository;
+  private final ThemeRegistry themeRegistry;
   private final InetAddress localHost;
 
   @Inject
-  LocalInstanceAnnouncer(MessageQueue messageQueue, MessageFlow messageFlow, Plugin plugin,
-      InstanceRepository instanceRepository, LocalInstanceProvider instanceProvider,
+  LocalInstanceAnnouncer(
+      MessageQueue messageQueue,
+      MessageFlow messageFlow,
+      Plugin plugin,
+      InstanceRepository instanceRepository,
+      LocalInstanceProvider instanceProvider,
+      SystemRepository systemRepository,
+      ThemeRegistry themeRegistry,
       @Named("LocalHost") InetAddress localHost) {
-    this.aliveQueue = messageQueue.declareFor(InstanceCreatedMessage.class)
-        .withTopic("scofu.instance");
+    this.aliveQueue =
+        messageQueue.declareFor(InstanceCreatedMessage.class).withTopic("scofu.instance");
     this.plugin = plugin;
     this.instanceRepository = instanceRepository;
     this.instanceProvider = instanceProvider;
+    this.systemRepository = systemRepository;
+    this.themeRegistry = themeRegistry;
     this.instanceFuture = new CompletableFuture<>();
     this.localHost = localHost;
-    messageFlow.subscribeTo(InstanceAvailabilityRequest.class)
+    messageFlow
+        .subscribeTo(InstanceAvailabilityRequest.class)
         .replyWith(InstanceAvailabilityReply.class)
         .withTopic("scofu.instance." + localHost.getHostName())
         .via(this::onInstanceAvailabilityRequest);
@@ -47,10 +60,21 @@ final class LocalInstanceAnnouncer implements Feature {
   @Override
   public void enable() {
     System.out.println("getting instance");
-    instanceProvider.get().thenAcceptAsync(instance -> {
-      instanceFuture.complete(instance);
-      aliveQueue.push(new InstanceCreatedMessage(instance));
-    });
+    systemRepository
+        .get()
+        .thenComposeAsync(
+            system -> {
+              themeRegistry.setDefaultTheme(
+                  themeRegistry
+                      .byName(system.theme())
+                      .orElseGet(() -> themeRegistry.byName("Vanilla").orElseThrow()));
+              return instanceProvider.get();
+            })
+        .thenAcceptAsync(
+            instance -> {
+              instanceFuture.complete(instance);
+              aliveQueue.push(new InstanceCreatedMessage(instance));
+            });
   }
 
   @Override
@@ -65,7 +89,8 @@ final class LocalInstanceAnnouncer implements Feature {
     if (event.isCancelled()) {
       return completedFuture(null);
     }
-    return instanceProvider.get()
+    return instanceProvider
+        .get()
         .thenApplyAsync(instance -> new InstanceAvailabilityReply(true, null, instance));
   }
 }
